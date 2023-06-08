ef7bc139d2f51592bbaf953ba25143ab2ca8aa60
Somesh 3168 <somesh3168@gmail.com>
2023-06-09 00:06:48 +0530

    added stars in tech exposure

--- src/sections/experience.jsx
+++ src/sections/experience.jsx
@@ -6,7 +6,7 @@ export default function Experience() {
     <div div className="card">
         <h3>EXPERIENCE</h3>
           <ul className='exp-list'>
-            <li><b>Sign In Scheduling (Formerly 10to8)</b> — <cite>Full Stack Developer</cite><p><small>JULY 2022 - PRESENT</small></p></li>
+            <li><b>Sign In Scheduling</b> — <cite>Full Stack Developer</cite><p><small>JULY 2022 - PRESENT</small></p></li>
             <li><b>Infosys</b> — <cite>Senior System Engineer</cite><p><small>MAY 2021 - JULY 2022</small></p></li>
             <li><b>Dhiyotech Pvt Ltd</b> — <cite>Python Developer</cite><p><small>NOV 2019 - MARCH 2021</small></p></li>
             <li><b>Magnet Brains</b> — <cite>Associate</cite><p><small>MARCH 2019 - OCT 2019</small></p></li>
--- src/sections/technicalExposure.jsx
+++ src/sections/technicalExposure.jsx
@@ -1,16 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'material-icons/iconfont/material-icons.css';
 
+
+
+const fillStar = `<span class='material-icons fill'>star</span>`
+const fadeStar = `<span class='material-icons fade'>star</span>`
+
+function getStarsOutOfFive(num) {
+  return fillStar.repeat(num)+fadeStar.repeat(5-num)
+}
+
+
 export default function TechnicalExposure() {
+  const [open, setOpen] = useState(true)
   return (
     <div div className="card">
-        <h3>TECHNICAL EXPOSURE</h3>
-        <ul className='exp-list'>
-            <li><b>Python</b><p><small>JULY 2022 - PRESENT</small></p></li>
-            <li><b>React JS</b><p><small>MAY 2021 - JULY 2022</small></p></li>
-            <li><b>Linux</b><p><small>JULY 2022 - PRESENT</small></p></li>
-            <li><b>Docker</b><p><small>MAY 2021 - JULY 2022</small></p></li>
-          </ul>
+      <h3>TECHNICAL EXPOSURE</h3>
+      <ul className='exp-list no-link'>
+        <li>
+          <a onClick={() => { setOpen(!open) }}><b>Python</b></a>
+          {!!open && <div className="show-box">
+
+            <p><small>JULY 2022 - PRESENT</small><br />
+              <small dangerouslySetInnerHTML={{ __html: getStarsOutOfFive(4) }}></small>
+            </p>
+          </div>}
+        </li>
+        <li>
+          <a onClick={() => { setOpen(!open) }}><b>React JS</b></a>
+          {!!open && <div className="show-box">
+            <p><small>JULY 2022 - PRESENT</small><br />
+              <small dangerouslySetInnerHTML={{ __html: getStarsOutOfFive(3) }}></small>
+            </p>
+          </div>}
+        </li>
+        <li>
+          <a onClick={() => { setOpen(!open) }}><b>Linux</b></a>
+          {!!open && <div className="show-box">
+            <p><small>JULY 2022 - PRESENT</small><br />
+              <small dangerouslySetInnerHTML={{ __html: getStarsOutOfFive(3) }}></small>
+            </p>
+          </div>}
+        </li>
+        <li>
+          <a onClick={() => { setOpen(!open) }}><b>Docker</b></a>
+          {!!open && <div className="show-box">
+            <p><small>JULY 2022 - PRESENT</small><br />
+              <small dangerouslySetInnerHTML={{ __html: getStarsOutOfFive(2) }}></small>
+            </p>
+          </div>}
+        </li>
+      </ul>
     </div>
   )
 }
